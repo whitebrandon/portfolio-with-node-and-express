f002ecd708532e88b5ebbe2f9c1264d782c4266c
Brandon White <white.brandonsean@gmail.com>
2019-09-29 10:22:32 -0400

    Added middleware to app.js

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const express = require('express');
-require('./data.json');
+const {projects} = require('./data.json');
+
+const app = express();
+
+const indexRoute = require('./routes');
+const aboutRoute = require('./routes/about.js');
+const projectRoutes = require('./routes/projects.js');
+
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+
+app.use("/", indexRoute);
+
+app.use("/about", aboutRoute);
+
+app.use("/projects", projectRoutes);
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000...');
+});
