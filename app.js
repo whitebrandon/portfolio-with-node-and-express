a882124a5800941d5fb60b7ea02e882765c33c49
Brandon White <white.brandonsean@gmail.com>
2019-10-03 19:31:20 -0400

    corrected syntax error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const indexRoute = require('./routes');
 const aboutRoute = require('./routes/about.js');
 const projectRoutes = require('./routes/projects.js');
 
-cont port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 
